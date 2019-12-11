049fb6aa254a8e51b381613c8079079d91ba391b
brandiherrera <bcjherrera@gmail.com>
2019-12-11 09:32:58 -0800

    revised config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: /*process.env.CLIENT_ORIGIN || */'https://brandiherrera-migraine-app-full-stack-capstone-react.now.sh/',
+    CLIENT_ORIGIN: /*process.env.CLIENT_ORIGIN || */'https://brandiherrera-migraine-app-full-stack-capstone-react.now.sh',
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
